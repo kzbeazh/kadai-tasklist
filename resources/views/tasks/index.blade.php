3a08053e563194fe260be5c92fb74b53679769a0
kzbeazh <kzbeazh@gmail.com>
2019-08-19 11:29:14 +0000

    lesson14-1 2nd

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,7 +10,6 @@
                 <thead>
                     <tr>
                         <th>id</th>
-                        <th>タスク作成者</th>
                         <th>ステータス</th>
                         <th>タスク</th>
                     </tr>
@@ -18,14 +17,12 @@
                 <tbody>
                     @foreach ($tasks as $task)
                     <tr>
-                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                         @if (\Auth::id() === $task->user_id)
-                            <td>あなた</td>
+                            <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                            <td>{{ $task->status }}</td>
+                            <td>{{ $task->content }}</td>
                         @else
-                            <td>他のユーザー</td>
                         @endif
-                        <td>{{ $task->status }}</td>
-                        <td>{{ $task->content }}</td>
                     </tr>
                     @endforeach
                 </tbody>
